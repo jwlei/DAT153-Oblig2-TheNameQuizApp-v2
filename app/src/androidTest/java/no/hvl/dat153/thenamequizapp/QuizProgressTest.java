a9c51ac8bef6422846a9c83d8dcf0bf28d4082bf
jwlei <joleiros@gmail.com>
2022-03-13 22:27:11 +0100

    QuizProgressTest

--- app/src/androidTest/java/no/hvl/dat153/thenamequizapp/QuizProgressTest.java
+++ app/src/androidTest/java/no/hvl/dat153/thenamequizapp/QuizProgressTest.java
@@ -3,14 +3,19 @@ package no.hvl.dat153.thenamequizapp;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasSibling;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.widget.RadioGroup;
 
 import androidx.test.core.app.ActivityScenario;
@@ -21,6 +26,9 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
 import androidx.test.runner.lifecycle.Stage;
 
+import junit.framework.AssertionFailedError;
+
+import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -53,21 +61,76 @@ public class QuizProgressTest {
         // velg radiobutton med tekst som er lik riktig person sitt navn
         RadioGroup radioGroup = quizActivity.getRadioGroup();
 
-        ViewInteraction button = Espresso.onView(ViewMatchers.withId(R.id.radio_btns));
-
         //onView(withId(R.id.quizAlternative1).matches(withText(correctName))).perform()
-        //onView(withId(R.id.quizAlternative1)).check(matches(withText(is(correctName)))).perform(click());
 
-        onView(withId(R.id.radio_btns)).check(matches(withText(is(correctName)))).perform(click());
+        try {
+            onView(withId(R.id.quizAlternative1)).check(matches(withText(correctName))).perform(click());
+        } catch (AssertionFailedError e) {
+        }
+
+        try {
+            onView(withId(R.id.quitAlternative2)).check(matches(withText(correctName))).perform(click());
+        } catch (AssertionFailedError e) {
+
+        }
+
+        try {
+            onView(withId(R.id.quizAlternative3)).check(matches(withText(correctName))).perform(click());
+        } catch (AssertionFailedError e) {
+        }
+
+
+
+
+
 
         // Click on submit
         onView(withId(R.id.submitBtn)).perform(click());
+
         onView(withId(R.id.endQuizBtn)).perform(click());
+
         onView(withId(R.id.showResult)).check(matches(withText("Score: 1")));
 
         // assert at score = score+1
+    }
+
+    @Test
+    public void wrongQuestion() {
+        // hent Person objektet som vises i bildet nå
+        // og ta tak i nåværende score
+
+        QuizActivity quizActivity = (QuizActivity) getInstance();
+
+        Person correctPerson = quizActivity.getCorrectPerson();
+        String correctName = correctPerson.getName();
 
 
+        // velg radiobutton med tekst som er lik riktig person sitt navn
+        RadioGroup radioGroup = quizActivity.getRadioGroup();
+
+        //onView(withId(R.id.quizAlternative1).matches(withText(correctName))).perform()
+
+        // click the wrong answer and get 0 in score
+        try {
+            onView(withId(R.id.quizAlternative1)).check(matches(not(withText(correctName))));
+        } catch (AssertionFailedError e) {
+        }
+
+        try {
+            onView(withId(R.id.quitAlternative2)).check(matches(not(withText(correctName))));
+        } catch (AssertionFailedError e) {
+
+        }
+
+
+        // Click on submit
+        onView(withId(R.id.submitBtn)).perform(click());
+
+        onView(withId(R.id.endQuizBtn)).perform(click());
+
+        onView(withId(R.id.showResult)).check(matches(withText("Score: 0")));
+
+        // assert at score = score+1
     }
 
     private Activity getInstance() {
